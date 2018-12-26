9e813e3556b71756b7bf28632ad3a8efacaaaa3b
kouya07 <luglio.11m44@gmail.com>
2018-12-26 22:48:39 +0900

    Deliveryクラス追加

--- app/src/main/java/com/example/onosystems/HomeActivity.java
+++ app/src/main/java/com/example/onosystems/HomeActivity.java
@@ -1,20 +1,29 @@
 package com.example.onosystems;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,14 +32,16 @@ public class HomeActivity extends AppCompatActivity
        implements SearchView.OnQueryTextListener, AdapterView.OnItemClickListener {
 
     public ListView listView;
-    public String[][] deliveryInfo;
+    public Delivery[] deliveryInfo = new Delivery[100];
     public List<Map<String, String>> list;
+    public JSONArray datalength;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
 
         toolbarView();
+        getDeliveries();
         reloadDeliveries();
         findDeliveries();
     }
@@ -53,28 +64,45 @@ public class HomeActivity extends AppCompatActivity
     }
 
     public void getDeliveries() {
-        //サーバからデータ受け取る
-        deliveryInfo = new String[][] {{"宛名1", "1/1", "日付1", "番号1", "住所1"}, {"宛名2", "1/2", "日付2", "番号2", "住所2"},
-                {"宛名3", "1/3", "日付3", "番号3", "住所3"}, {"宛名4", "1/4", "日付4", "番号4", "住所4"}};
+        //本来はサーバからデータ受け取る
+        // 各要素ごとにインスタンス化
+        for(int i = 0; i < deliveryInfo.length; i++) {
+            deliveryInfo[i] = new Delivery();
+        }
+
+        try {
+            JSONObject json = new JSONObject("{\"data\":[{\"name\":\"001\", \"time\":\"001\", \"day\":\"1\", \"slipNumber\":\"1111\", \"address\":\"1001\"}," +
+                    " {\"name\":\"002\", \"time\":\"002\", \"day\":\"2\", \"slipNumber\":\"1112\", \"address\":\"1002\"}, " +
+                    "{\"name\":\"003\", \"time\":\"003\", \"day\":\"3\", \"slipNumber\":\"1113\", \"address\":\"1003\"}]}");
+
+            datalength = json.getJSONArray("data");
+
+            for (int i = 0; i < datalength.length(); i++) {
+                deliveryInfo[i].name = json.getJSONArray("data").getJSONObject(i).getString("name");
+                deliveryInfo[i].day = json.getJSONArray("data").getJSONObject(i).getInt("day");
+                deliveryInfo[i].slipNumber = json.getJSONArray("data").getJSONObject(i).getLong("slipNumber");
+                deliveryInfo[i].address = json.getJSONArray("data").getJSONObject(i).getString("address");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     public void reloadDeliveries() {
-        getDeliveries();
-
         list = new ArrayList<Map<String, String>>();
-        for (int i = 0; i < deliveryInfo.length; i++) {
+        for (int i = 0; i < datalength.length(); i++) {
             Map<String, String> item = new HashMap<String, String>();
-            item.put("address", deliveryInfo[i][0]);
-            item.put("time", deliveryInfo[i][1]);
-            item.put("slipNumber", deliveryInfo[i][3]);
-            item.put("deliveryAddress", deliveryInfo[i][4]);
+            item.put("name", deliveryInfo[i].name);
+            item.put("day", String.valueOf(deliveryInfo[i].day));
+            item.put("slipNumber", String.valueOf(deliveryInfo[i].slipNumber));
+            item.put("address", deliveryInfo[i].address);
             list.add(item);
         }
 
         SimpleAdapter adapter = new SimpleAdapter(this,
                 list, // 使用するデータ
                 R.layout.list_layout, // 自作したレイアウト
-                new String[]{"address", "time", "slipNumber", "deliveryAddress"}, // どの項目を
+                new String[]{"name", "day", "slipNumber", "address"}, // どの項目を
                 new int[]{R.id.addressText, R.id.timeText, R.id.slipNumberText, R.id.deliveryAddressText} // どのidの項目に入れるか
         );
         listView = findViewById(R.id.listView);
@@ -119,4 +147,18 @@ public class HomeActivity extends AppCompatActivity
     public void receivableSelect() {
     }
 
+}
+
+class Delivery {
+    long slipNumber;
+    String name;
+    String address;
+    long slip_from;
+    int time;
+    int day;
+    int receivable_status;
+    int delivered_tatus;
+    double lat;
+    double lng;
+    boolean visible;
 }
