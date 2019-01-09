7f4ef5af2eb347034fa5806de7d4d73e5f731df1
anipyon <32477143+anipyon@users.noreply.github.com>
2019-01-10 08:03:33 +0900

    マップ、住所からピンたて出来る様に変更

--- app/src/main/java/com/example/test/MapsActivity.java
+++ app/src/main/java/com/example/test/MapsActivity.java
@@ -1,11 +1,14 @@
 package com.example.takamaru.test;
-
+import android.location.Geocoder;
+import android.location.Address;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -14,7 +17,12 @@ import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 class Deliver {
+    String name;
     String address;
     int time;
     int deliverd_status;
@@ -36,6 +44,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if(mMap != null) {
+            //mMap.setMyLocationEnabled(true);
+        }
+
     }
 
 
@@ -53,32 +66,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // ひとまず許しておいてほしいゾーン
         // 簡易的なマーカーのデータを作成
-        Deliver[] deliver = new Deliver[3];
+        Deliver[] deliver = new Deliver[4];
         for(int i = 0; i < deliver.length; i++) {
             deliver[i] = new Deliver();
         }
+        int maxResults = 1;
+        String place = "高知県香美市土佐山田町宮ノ口１８５";
 
-        deliver[0].address = "あっち";
+        deliver[0].name = "高知工科大学";
+        deliver[0].address = "高知県香美市土佐山田町宮ノ口１８５";
         deliver[0].time = 15;
         deliver[0].deliverd_status = 0;
         deliver[0].visible = 0;
-        deliver[0].lat = 33.621652;
-        deliver[0].lng = 133.719034;
 
-        deliver[1].address = "こっち";
+
+        deliver[1].name = "リトルガーデン庭園喫茶";
+        deliver[1].address = "高知県香美市土佐山田町佐古藪２８６ー２９";
         deliver[1].time = 18;
         deliver[1].deliverd_status = 1;
         deliver[1].visible = 0;
-        deliver[1].lat = 33.620553;
-        deliver[1].lng = 133.722446;
 
-        deliver[2].address = "そっち";
+        deliver[2].name = "おおぞら";
+        deliver[2].address = "高知県香美市土佐山田町佐古藪１７２";
         deliver[2].time = 10;
         deliver[2].deliverd_status = 2;
         deliver[2].visible = 0;
-        deliver[2].lat = 33.617658;
-        deliver[2].lng = 133.718766;
+
+
+        deliver[3].name = "片地小学校";
+        deliver[3].address = "高知県香美市土佐山田町宮ノ口９";
+        deliver[3].time = 10;
+        deliver[3].deliverd_status = 2;
+        deliver[3].visible = 0;
+
         // ここまで
+        Geocoder gcoder = new Geocoder(this, Locale.getDefault());
+        List<Address> lstAddr;
+        try {
+            for(int i = 0; i < deliver.length ; i++) {
+                lstAddr = gcoder.getFromLocationName(deliver[i].address, maxResults);
+                Address addr = lstAddr.get(0);
+                deliver[i].lat = (addr.getLatitude());
+                deliver[i].lng = (addr.getLongitude());
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         final Intent intent_CourierDeliveryDetail = new Intent(getApplication(), CourierDeliveryDetail.class);
         // 画面上にマップを作成
         mMap = googleMap; //マップ
@@ -97,8 +132,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         for(int i = 0; i < deliver.length; i++){ // ピンごとに設定を変更
             option[i] = new MarkerOptions();
             option[i].position(points[i]);
-            option[i].title(deliver[i].address);
-            option[i].snippet("配達希望時間帯" + String.valueOf(deliver[i].time));
+            option[i].title(deliver[i].name);
+            option[i].snippet(String.valueOf(deliver[i].time) + "時頃");
+            //option[i].snippet(String.valueOf(deliver[i].lat) + ":" + String.valueOf(deliver[i].lng));
             if(deliver[i].deliverd_status == 0) {
                 option[i].icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
             }else if(deliver[i].deliverd_status == 1) {
@@ -121,7 +157,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             });
         }
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(points[0], 15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(points[3], 15));
 
 
     }
