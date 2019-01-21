99aeb7fec9d7481e11a848ae15e7f7ca40581cae
200341g <200341g@ugs.kochi-tech.ac.jp>
2019-01-21 14:46:05 +0900

    通信関連

--- app/src/main/java/com/example/onosystems/Request.java
+++ app/src/main/java/com/example/onosystems/Request.java
@@ -1,4 +1,37 @@
-package com.example.onosystems;
+ package com.example.onosystems;
 
-public class Request {
+import android.os.AsyncTask;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Request extends AsyncTask<String, String, String> {
+    interface CallBack {
+        void fetchResult(String result);
+    }
+
+    private CallBack callBack;
+
+    public void setCallBack(CallBack callBack) {
+        this.callBack = callBack;
+    }
+
+    // 第一引数がURL、第二引数がbody
+    @Override
+    protected String doInBackground(String... strings) {
+        URL url = null;
+        try {
+            url = new URL(strings[0]);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        String body = strings[1];
+        return CallAPI.post(url, body);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        callBack.fetchResult(result);
+    }
 }
--- app/src/main/java/com/example/onosystems/RequestLogin.java
+++ app/src/main/java/com/example/onosystems/RequestLogin.java
@@ -1,4 +1,18 @@
 package com.example.onosystems;
 
-public class RequestLogin {
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+
+/*
+ *  ログイン時にサーバとの通信で使用する
+ */
+
+public class RequestLogin extends Request {
+    CookieManager manager;
+    RequestLogin() {
+        // Cookieが発行される通信の前(要するにLoginの際)にCookieManagerを生成してセットする
+        manager = new CookieManager();
+        manager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        CallAPI.setCookieManager(manager);
+    }
 }
