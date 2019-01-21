8624832ae516243e87afbd1a18f0ddeac53b79ac
the96 <aar30px2@gmail.com>
2019-01-21 21:23:24 +0900

    入力情報を使ってログインして、トークンをサーバに投げて画面遷移する

--- app/src/main/java/com/example/onosystems/LoginActivity.java
+++ app/src/main/java/com/example/onosystems/LoginActivity.java
@@ -1,15 +1,24 @@
 package com.example.onosystems;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
@@ -37,22 +46,19 @@ import java.sql.Driver;
 
 
 /**
- * A login screen that offers login via email/password.
+ * メールアドレスとパスワードでログインするログイン画面
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements RequestLogin.CallBack{
 
     private String loginEmail;
     private String loginPassword;
     private String token;
+    public static final String URL_ORIGIN = "http://www.onosystems.work/aws/";
     private int customer_id = 0;
     private int driver_id = 0;
     private SharedPreferences sharedPreferences;
     private String url = "http://54.92.85.232/aws/Login";
-
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
 
     // UI references.
     private AutoCompleteTextView mEmailView;
@@ -63,9 +69,11 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PostAsync.initializeCallAPI();
         setContentView(R.layout.activity_login);
 
-//        sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+        // ログイン情報を保存するためのもの
+        sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
 //        autoLogin();
 
         mEmailView = findViewById(R.id.loginId);
@@ -80,41 +88,27 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 loginEmail = mEmailView.getText().toString();
                 loginPassword = mPasswordView.getText().toString();
-                login(loginEmail, loginPassword);
+                if (!isEmpty(loginEmail)) {
+                    mEmailView.setError("メールアドレスを入力してください");
+                    return;
+                } else if (!isEmailValid(loginEmail)) {
+                    mEmailView.setError("@が含まれていません");
+                    return;
+                } else if (!isEmpty(loginPassword)) {
+                    mPasswordView.setError("パスワードを入力してください");
+                    return;
+                } else {
+                    login(loginEmail, loginPassword);
+                }
 
-                Intent intent = new Intent(LoginActivity.this, CourierHomeActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(LoginActivity.this, CourierHomeActivity.class);
+//                startActivity(intent);
 
 
 
 
 //                FirebaseMessaging.getInstance().subscribeToTopic("test");
 
-//                Toast toast = Toast.makeText(LoginActivity.this, "test", Toast.LENGTH_SHORT);
-//                toast.show();
-/*
-                if (isEmpty(loginEmail)) {
-                    if (isEmpty(loginPassword)) {
-//                        login(loginEmail, loginPassword);
-
-                    } else {
-                    AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this)
-                        .setMessage("パスワードを入力してください")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                            }
-                        }).show();
-                    }
-                } else {
-                    AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this)
-                            .setMessage("メールアドレスを入力してください")
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                }
-                            }).show();
-                }*/
 
             }
         });
@@ -165,50 +159,95 @@ public class LoginActivity extends AppCompatActivity {
             body.put("password", password);
             body.put("token", token);
             System.out.println(body.toString());
-            
-            new PostFirebaseTokenToServer().execute("url", body.toString());
+            PostAsync postAsync = new PostAsync();
+            postAsync.setRef(new PostAsync.Callback() {
+                @Override
+                public void callback(String result) {
+                    try {
+                        JSONObject json = new JSONObject(result);
+                        int TmpCustomerId = json.optInt("customer_id");
+                        int TmpDriverId = json.optInt("driver_id");
+                        if (TmpCustomerId != 0) {
+                            customer_id = TmpCustomerId;
+                        } else if (TmpDriverId != 0) {
+                            driver_id = TmpDriverId;
+                        } else {
+                            customer_id = 0;
+                            driver_id = 0;
+                        }
+                        transitionActivity();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            postAsync.execute(URL_ORIGIN + "Login", body.toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
         /*try {
             JSONObject loginJson = new JSONObject();
             loginJson.put("id", id);
             loginJson.put("password", password);
 
             String loginInfo = loginJson.toString();
-            SampleLogin sampleLogin = new SampleLogin();
-            sampleLogin.execute(url, loginInfo);
-            String result = sampleLogin.doInBackground(url, loginInfo);
+            sendRequest(loginInfo);
 
-            JSONObject jsonObject = new JSONObject(result);
-            customer_id = jsonObject.getInt("customer_id");
-            driver_id = jsonObject.getInt("driver_id");
-
-            transitionActivity();
+//            // テスト用
+            JSONObject test = new JSONObject();
+            for (String credential : DUMMY_CUSTOMER) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(loginEmail) && pieces[1].equals(loginPassword)) {
+                    test.put("customer_id",1);
+                    this.customer_id = test.getInt("customer_id");
+                    this.driver_id = test.getInt("driver_id");
+                    String s = String.valueOf(customer_id);
+                }
+            }
+            for (String credential : DUMMY_DRIVER) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(loginEmail) && pieces[1].equals(loginPassword)) {
+                    test.put("driver_id", 1);
+//                            this.customer_id = test.getInt("customer_id");
+                    this.driver_id = test.getInt("driver_id");
+                    String s = String.valueOf(driver_id);
+                }
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
-        }*/
+        }
 
-    }
+        String s = String.valueOf(customer_id);
+//        Toast toast = Toast.makeText(LoginActivity.this, s, Toast.LENGTH_SHORT);
+//        toast.show();
+
+        if (customer_id == 0 && driver_id == 0) {
+            AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this)
+                    .setMessage("ログインに失敗しました" + customer_id)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            mPasswordView.setText("");
+                        }
+                    }).show();
+        } else {
+            transitionActivity();
+        }
+    }*/
 
     // ログイン成功時にホーム画面へ遷移する
-    /*public void transitionActivity() {
-        FirebaseMessaging.getInstance().subscribeToTopic("test");
-        sendToken();
+    public void transitionActivity() {
+//        FirebaseMessaging.getInstance().subscribeToTopic("test");
+//        sendToken();
 
         // ログイン情報を端末に保存
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+        /*SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("account", loginEmail);
+
         editor.putString("pass", loginPassword);
-        editor.apply();
-
-        Toast toast = Toast.makeText(LoginActivity.this, "trance", Toast.LENGTH_SHORT);
-                toast.show();
-
-        Intent intent2 = new Intent(getApplication(), CustomerHomeActivity.class);
-        startActivity(intent2);
-
+        editor.apply();*/
 
         if (customer_id != 0) {
             // 消費者側
@@ -222,10 +261,8 @@ public class LoginActivity extends AppCompatActivity {
             intent.putExtra("driver_id", driver_id);
             intent.putExtra("password", loginPassword);
             startActivity(intent);
-        } else {
-            mPasswordView.setError("Error");
         }
-    }*/
+    }
 
     public void createNewAccountActivity() {
         Intent intent = new Intent(getApplication(), NewAccountActivity.class);
@@ -239,5 +276,66 @@ public class LoginActivity extends AppCompatActivity {
         return false;
     }
 
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+
+    public void sendRequest(String json) {
+        showProgress(true);
+
+        RequestLogin requestLogin = new RequestLogin();
+        requestLogin.setCallBack(this);
+        requestLogin.execute(url, json);
+    }
+
+    // テスト用
+    public static final String[] DUMMY_CUSTOMER = new String[]{
+            "@1:1","kut@gmail.com:onosystems", "kut2@gmail.com:onosystems2", "kut3@gmail.com:onosystems3"
+    };
+    public static final String[] DUMMY_DRIVER = new String[]{
+            "driver@gmail.com:driver","@2:2"
+    };
+
+    //    @Override
+    public void fetchResult(String result) {
+        try {
+            // テスト用
+            // Simulate network access.
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+        }
+
+        showProgress(false);
+
+    }
+
+    // progressの表示とログインフォームの非表示.
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
 }
 
