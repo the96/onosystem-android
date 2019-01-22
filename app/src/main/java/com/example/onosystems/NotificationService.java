a644dbeb9cf311bd8b40f5dc532b9463cf6245d6
the96 <aar30px2@gmail.com>
2019-01-22 23:54:01 +0900

    フォアグラウンド時に通知を読み上げる機能を実装

--- app/src/main/java/com/example/onosystems/NotificationService.java
+++ app/src/main/java/com/example/onosystems/NotificationService.java
@@ -8,19 +8,97 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class NotificationService extends FirebaseMessagingService {
+import java.util.Map;
+import java.util.UUID;
+
+public class NotificationService extends FirebaseMessagingService implements TextToSpeech.OnInitListener {
     private static final String NOTIFICATION_CHANNEL_ID = "ONOSYSTEMS_NOTIFICATION";
     private static final String NOTIFICATION_CHANNEL_NAME = "ONOSYSTEMS_NOTIFICATION";
+    private static final String CUSTOMER_USER = "customer";
+    private static final String DRIVER_USER = "driver";
+    private static final int NOTICE_RECEIVABLE = 1;
+    private static final int NOTICE_NOT_RECEIVABLE = 2;
+    private static final int NOTICE_CHANGE_DATE = 3;
+    private static final int NOTICE_NEAR_DRIVER = 1;
+    private static TextToSpeech textToSpeech;
+    private static NoticeData data;
+    private boolean initialized = false;
+
+
+    class NoticeData {
+        private String title, body, userType;
+        private int type;
+        NoticeData(RemoteMessage.Notification notification, Map<String, String> payload) {
+            this.title = notification.getTitle();
+            this.body = notification.getBody();
+            userType = payload.get("user_type");
+            if (userType == null) {
+//            userTypeがなかった場合
+//            このコードではテストのため毎回読み上げる
+                userType = DRIVER_USER;
+            }
+            try {
+                this.type = Integer.parseInt(payload.get("notice_type"));
+            } catch (NumberFormatException e) {
+//            parseIntに失敗した場合、debug用に毎回読み上げる値をセット
+//            このコードではテストのため毎回読み上げる
+                this.type = NOTICE_NOT_RECEIVABLE;
+            }
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getUserType() {
+            return userType;
+        }
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        String title = remoteMessage.getNotification().getTitle();
-        String body = remoteMessage.getNotification().getBody();
+        data = new NoticeData(remoteMessage.getNotification(), remoteMessage.getData());
+        if (needSpeech(data)) {
+            if (!initialized) {
+                textToSpeech = new TextToSpeech(this, this);
+                textToSpeech.setSpeechRate(1.0f);
+                textToSpeech.setPitch(1.0f);
+//            引数sは与えられたUUID
+//            textToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+//                @Override
+//                public void onStart(String s) {
+//                    System.out.println("読み上げ開始: " + s);
+//                }
+//
+//                @Override
+//                public void onDone(String s) {
+//                    System.out.println("読み上げ終了: " + s);
+//                }
+//
+//                @Override
+//                public void onError(String s) {
+//                    System.out.println("読み上げ中にエラーが発生しました。");
+//                    System.out.println(s);
+//                }
+//            });
+            } else {
+                speechText(data.getBody());
+            }
+        }
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationCompat.Builder mBuilder;
@@ -32,13 +110,13 @@ public class NotificationService extends FirebaseMessagingService {
             notificationManager.createNotificationChannel(channel);
             mBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
                     .setSmallIcon(R.drawable.onosystem_logo_mini)
-                    .setContentTitle(title)
-                    .setContentText(body);
+                    .setContentTitle(data.getTitle())
+                    .setContentText(data.getBody());
         } else {
             mBuilder = new NotificationCompat.Builder(this)
                     .setSmallIcon(R.drawable.onosystem_logo_mini)
-                    .setContentTitle(title)
-                    .setContentText(body);
+                    .setContentTitle(data.getTitle())
+                    .setContentText(data.getBody());
         }
 
         Intent openIntent = new Intent(this,  LoginActivity.class);
@@ -48,5 +126,26 @@ public class NotificationService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, openIntent, PendingIntent.FLAG_ONE_SHOT);
         mBuilder.setContentIntent(pendingIntent);
         notificationManager.notify(0, mBuilder.build());
+
+    }
+
+    @Override
+    public void onInit(int i) {
+        if (i != TextToSpeech.SUCCESS) {
+            System.out.println("TextToSpeech initializing failed");
+        } else {
+            initialized = true;
+            speechText(data.getBody());
+        }
+    }
+
+    private boolean needSpeech(NoticeData notice) {
+        return notice.getUserType().equals(DRIVER_USER) && notice.getType()!= NOTICE_RECEIVABLE;
+    }
+
+    private void speechText(String text) {
+        if (initialized && text.length() > 0) {
+            textToSpeech.speak(text,  TextToSpeech.QUEUE_ADD, null, UUID.randomUUID().toString());
+        }
     }
 }
