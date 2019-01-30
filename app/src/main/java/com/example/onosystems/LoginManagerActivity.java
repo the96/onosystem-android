f91e8cca701f39feea935ca26f966f790a4dcabc
200341g <200341g@ugs.kochi-tech.ac.jp>
2019-01-30 22:51:32 +0900

    管理者ログインページ

--- app/src/main/java/com/example/onosystems/LoginManagerActivity.java
+++ app/src/main/java/com/example/onosystems/LoginManagerActivity.java
@@ -2,6 +2,8 @@ package com.example.onosystems;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class LoginManagerActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginManagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_manager);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(PostURL.getManagerURL());
     }
 }
